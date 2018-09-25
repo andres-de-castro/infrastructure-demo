e6d5c65bccf94f35b264b328756942338dfbe51a
Andres De Castro <andres.decastro@ampleorganics.com>
2018-09-25 14:32:26 -0400

    re enabled cache

--- snaptravel/config/default.py
+++ snaptravel/config/default.py
@@ -1,17 +1,17 @@
 import os
 
 class DefaultConfig(object):
-    REDIS_HOST = 'redis'
     DEBUG = False
-    if os.getenv('REDIS_URL'):
-        REDIS_URL = os.env('REDIS_URL')
-    REDIS_PORT = 6379
-    REDIS_DB = 0
-    REDIS_MAX_CONNECTIONS = 5
+
     SECRET_KEY = os.getenv('SECRET_KEY', b'\x07X\x1e\x9bjE!\xcd\x1c\x7f\x82P\x87\xc5+\x11\xdd\x96\x1a!\xc3\t\r\xad')
     
-    REDIS_HOST = 'localhost'
-    REDIS_URL = 'redis://{0}:6379/{1}'.format(REDIS_HOST, REDIS_DB)
+    REDIS_PORT = 6379
+    REDIS_DB = 0
+    REDIS_MAX_CONNECTIONS = 5    
+    REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+    if os.getenv('REDIS_URL'):
+        REDIS_URL = os.getenv('REDIS_URL', 'redis://{0}:6379/{1}'.format(REDIS_HOST, REDIS_DB))
+
 
     if os.getenv('production'):
         flask_caching_config = {
